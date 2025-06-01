cb56e4bbccc24a673798c0e51175c26555ce38e8
S24796 <s24796@pjwstk.edu.pl>
2025-06-01 09:04:56 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,33 @@ public class Main {
     public static void main(String[] args) {
         BibliotekaMuzyczna biblioteka = new BibliotekaMuzyczna();
 
-        // Dodawanie utworów
+
         biblioteka.dodajUtwor("Imagine - John Lennon");
         biblioteka.dodajUtwor("Bohemian Rhapsody - Queen");
         biblioteka.dodajUtwor("Let It Be - The Beatles");
 
-        // Wyświetlanie wszystkich utworów
+
         biblioteka.wyswietlUtwory();
 
-        // Tworzenie playlist
+
         biblioteka.utworzPlayliste("Rock");
-        biblioteka.utworzPlayliste("Relaks");
+        biblioteka.utworzPlayliste("pop");
 
-        // Dodawanie utworów do playlist
+
         biblioteka.dodajUtworDoPlaylisty("Rock", "Bohemian Rhapsody - Queen");
-        biblioteka.dodajUtworDoPlaylisty("Relaks", "Imagine - John Lennon");
+        biblioteka.dodajUtworDoPlaylisty("pop", "Imagine - John Lennon");
 
-        // Wyświetlenie playlist
+
         biblioteka.wyswietlPlayliste("Rock");
-        biblioteka.wyswietlPlayliste("Relaks");
+        biblioteka.wyswietlPlayliste("pop");
 
-        // Wyszukiwanie
+
         biblioteka.wyszukajUtwory("let");
 
-        // Usuwanie utworu
+
         biblioteka.usunUtwor("Let It Be - The Beatles");
 
-        // Po usunięciu
+
         biblioteka.wyswietlUtwory();
         biblioteka.wyswietlWszystkiePlaylisty();
     }
